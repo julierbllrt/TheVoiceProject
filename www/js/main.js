b55388428002c176109ef3128d3dcc08691b6f1b
Pierre Lalin <pierre.lalin@isen.yncrea.fr>
2017-11-08 10:19:29 +0100

    jspierre

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,60 @@
-$( document ).ready(function() {
-	$('#action').click(function(){ 
+<script type="text/javascript" charset="utf-8">
+			
+			
+			document.addEventListener("deviceready", onDeviceReady, false);
+			
+			var currentRow;
+			// Populate the database
+			//
+			function populateDB(tx) {
+				//alert("going populateDB");
+				tx.executeSql('DROP TABLE IF EXISTS PICTOS');
+				tx.executeSql('CREATE TABLE IF NOT EXISTS PICTOS (nom TEXT NOT NULL, picto TEXT NOT NULL)');
+				tx.executeSql('INSERT INTO PICTOS(nom,picto) VALUES ("1erpicto", "img/imageProduit1.png")');
+				tx.executeSql('INSERT INTO PICTOS(nom,picto) VALUES ("2emepicto", "img/imageProduit2.jpg")');
+			}
+			
+			// Query the database
+			//
+			function queryDB(tx) {
+				//alert("going queryDB");
+				tx.executeSql('SELECT * FROM PICTOS', [], querySuccess, errorCB);
+			}
 
-		window.location.href='../Action.html';
+			// Query the success callback
+			//
+			function querySuccess(tx, results) {
+				//alert("going querySuccess");
+				var imgPicto="";
+				var len = results.rows.length;
+				for (var i = 0; i < len; i++) {
+					imgPicto +='<img class="picto" src="'+ results.rows.item(i).picto + '">';
+					document.getElementById("picto"+i).innerHTML = imgPicto;
+					imgPicto="";
+				}
+				
+			}
+			
+			// Transaction error callback
+			//
+			function errorCB(err) {
+				alert("Error processing SQL: "+err.code);
+			}
 
-	});
-});
+			// Transaction success callback
+			//
+			function successCB() {
+				//alert("going successCB");
+				var db = window.openDatabase("Database", "1.0", "Picto Demo", 200000);
+				db.transaction(queryDB, errorCB);
+			}
+
+			 // Cordova is ready
+			//
+			function onDeviceReady() {
+				//alert("going OnDeviceReady");
+				var db = window.openDatabase("Database", "1.0", "Picto Demo", 200000);
+				db.transaction(populateDB, errorCB, successCB);
+			}
+			
+      </script>
