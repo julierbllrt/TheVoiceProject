053a22d84915b331d25d68439e3f49afadc1acad
julierbllrt <julie.robillart@isen.yncrea.fr>
2017-11-14 11:06:44 +0100

    speech sur ordi

--- www/js/speech.js
+++ www/js/speech.js
@@ -1,20 +1,30 @@
-document.addEventListener('deviceready', function () {
-	
-	TTS.speak({
-		text: 'Bonjour',
-		locale: 'fr-FR',
-		rate: 1
-	}, function () {
-		console.log('OK !');
-	}, function (reason) {
-		console.log(reason);
-	});
-	
-},false);
+/* document.addEventListener('deviceready', parler(), false);
+
+
+function parler() {
+       
+    TTS
+        .speak({
+            text: 'hello, world!',
+            locale: 'en-GB',
+            rate: 0.75
+        }, function () {
+            alert('success');
+        }, function (reason) {
+            alert(reason);
+        });
+}
+ */
+
+//document.addEventListener('deviceready', parler(),false);
 
 function parler(){
-	alert("parler");
-	var text=document.getElementById("saisie");
-	alert(text.value);
-	TTS.speak({text: text.value});
+	//alert("parler");
+	var word=document.getElementById("saisie");
+	var u = new SpeechSynthesisUtterance();
+       var txt = "";
+       txt = word.value;
+       u.text = txt;
+       u.lang = 'fr-FR';
+       speechSynthesis.speak(u);
 }
