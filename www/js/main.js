3d05c3e116b5c325e15e21933a8d82e06cddec85
Pierre Lalin <pierre.lalin@isen.yncrea.fr>
2017-11-14 11:45:38 +0100

    modification tableau pictos presentation 14/11/17

--- www/js/main.js
+++ www/js/main.js
@@ -10,30 +10,42 @@ function populateDB(tx) {
 	tx.executeSql('DROP TABLE IF EXISTS PICTOS');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS PICTOS (id TEXT NOT NULL, picto TEXT NOT NULL, mot TEXT NOT NULL)');
 	var q = 'INSERT INTO PICTOS(id,picto,mot) VALUES (?, ?, ?)';
-	var dataArray = [{id: "1", picto: "img/1.png", mot: "un"}, 
-					{id: "2", picto: "img/2.png", mot: "deux"},
-					{id: "3", picto: "img/3.png", mot: "trois"},
-					{id: "4", picto: "img/4.png", mot: "quatre"},
-					{id: "5", picto: "img/0.png", mot: "zero"},
-					{id: "6", picto: "img/1_1.png", mot: "un"},
-					{id: "7", picto: "img/1-2-3.png", mot: "un, deux, trois"},
-					{id: "8", picto: "img/1-4.png", mot: "un quart"},
-					{id: "9", picto: "img/2 centimes.png", mot: "deux centimes"},
-					{id: "10", picto: "img/2 dé.png", mot: "deux"},
-					{id: "11", picto: "img/2 euros.png", mot: "deux euros"},
-					{id: "12", picto: "img/2_1.png", mot: "deux"},
-					{id: "13", picto: "img/3 dé.png", mot: "trois"},
-					{id: "14", picto: "img/3_1.png", mot: "trois"},
-					{id: "15", picto: "img/3-4.png", mot: "trois quarts"},
-					{id: "16", picto: "img/4 dé.png", mot: "quatre"},
-					{id: "17", picto: "img/4_1.png", mot: "quatre"},
-					{id: "18", picto: "img/5 dé.png", mot: "dé"},
+	var dataArray = [{id: "1", picto: "img/je.png", mot: "je"}, 
+					{id: "2", picto: "img/avoir_faim.png", mot: "avoir faim"},
+					{id: "3", picto: "img/vouloir.png", mot: "vouloir"},
+					{id: "4", picto: "img/frites.png", mot: "frites"},
+					{id: "5", picto: "img/du.png", mot: "du"},
+					{id: "6", picto: "img/poulet.png", mot: "poulet"},
+					{id: "7", picto: "img/avec.png", mot: "avec"},
+					{id: "8", picto: "img/une.png", mot: "une"},
+					{id: "9", picto: "img/salade.png", mot: "salade"},
+					{id: "10", picto: "img/eau.png", mot: "eau"},
+					{id: "11", picto: "img/et.jpg", mot: "et"},
+					{id: "12", picto: "img/0.png", mot: "zeros"},
+					{id: "13", picto: "img/1.png", mot: "un"},
+					{id: "14", picto: "img/2.png", mot: "deux"},
+					{id: "15", picto: "img/3.png", mot: "trois"},
+					{id: "16", picto: "img/4.png", mot: "quatre"},
+					{id: "17", picto: "img/5.png", mot: "cinq"},
+					{id: "18", picto: "img/2 euros.png", mot: "deux euros"},
 					{id: "19", picto: "img/5 euros.png", mot: "cinq euros"},
-					{id: "20", picto: "img/5.png", mot: "cinq"},
-					{id: "21", picto: "img/6.png", mot: "six"},
-					{id: "22", picto: "img/7.png", mot: "sept"},
-					{id: "23", picto: "img/8.png", mot: "huit"},
-					{id: "24", picto: "img/9.png", mot: "neuf"},
+					{id: "20", picto: "img/plus.png", mot: "plus"},
+					{id: "21", picto: "img/plus.png", mot: "plus"},
+					{id: "22", picto: "img/plus.png", mot: "plus"},
+					{id: "23", picto: "img/plus.png", mot: "plus"},
+					{id: "24", picto: "img/plus.png", mot: "plus"},
+					{id: "25", picto: "img/plus.png", mot: "plus"},
+					{id: "26", picto: "img/plus.png", mot: "plus"},
+					{id: "27", picto: "img/plus.png", mot: "plus"},
+					{id: "28", picto: "img/plus.png", mot: "plus"},
+					{id: "29", picto: "img/plus.png", mot: "plus"},
+					{id: "30", picto: "img/plus.png", mot: "plus"},
+					{id: "31", picto: "img/plus.png", mot: "plus"},
+					{id: "32", picto: "img/plus.png", mot: "plus"},
+					{id: "33", picto: "img/plus.png", mot: "plus"},
+					{id: "34", picto: "img/plus.png", mot: "plus"},
+					{id: "35", picto: "img/plus.png", mot: "plus"},
+					{id: "36", picto: "img/plus.png", mot: "plus"},
 					];
 	for (var i=0; i<dataArray.length; i++){
 		tx.executeSql(q, [dataArray[i].id, dataArray[i].picto, dataArray[i].mot])
@@ -54,7 +66,7 @@ function querySuccessStart(tx, results) {
 	var imgPicto="";
 	var len = results.rows.length;
 	for (var i = 0; i < len; i++) {
-		imgPicto +='<img id="' + results.rows.item(i).id + '" class="picto" src="' + results.rows.item(i).picto + '">';
+		imgPicto +='<img id="' + results.rows.item(i).id + '" class="picto" src="' + results.rows.item(i).picto + '"><p>' + results.rows.item(i).mot + '</p>';
 		document.getElementById("picto"+i).innerHTML = imgPicto;
 		imgPicto="";
 	}
