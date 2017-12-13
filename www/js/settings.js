6794bd452191108d6a0a6e47b05914abade43a85
julierbllrt <julie.robillart@isen.yncrea.fr>
2017-12-13 17:35:53 +0100

    parametre fonctionnel

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,11 +1,19 @@
+var nom =document.getElementById('nom');
+var prenom =document.getElementById('prenom');
+//var birthday = document.getElementById('birthday');
+var tel = document.getElementById('tel');
+var des = document.getElementById('description');
+var logOb;
+i=0;
+
+
 document.addEventListener('deviceready', onDeviceReady, false);
 document.getElementById("set").addEventListener("touchend", function(){
-	var nom =document.getElementById('nom');
-	alert(nom.value);
-	writeLog(nom.value);		
+	i=0;
+	var  string = nom.value + "\n" + prenom.value + "\n" + tel.value + "\n" + des.value + "\n";
+	writeLog(string);
+	alert('Les nouvelles informations on été enregistré');
 });
-
-var logOb;
 
 function fail(e) {
 	console.log("FileSystem Error");
@@ -13,47 +21,59 @@ function fail(e) {
 }
    
 function onDeviceReady() {
-	alert("devideready function");
-	
 	window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dir) {
-		alert("window.truc function");
 		console.log(dir.nativeURL);
 		console.log("got main dir", dir);
 		dir.getFile("log.txt", {create:true, exclusive: false}, function(file) {
 			console.log("got the file", file);
-			logOb = file;
-			writeLog("AppStart");			
+			logOb = file;		
+			console.log('getfile');
+			readLog();	
 		});
 	});
-
 }
 
 
 function writeLog(str) {
-	alert("in writeLog ");
+	console.log(str);
 	if(!logOb){
-		alert("echec log "+str);
+		return;
 	} 
-	var log =str+" suc.\n"
-	alert("going to log "+log);
-	logOb.createWriter(function(fileWriter) {
-		fileWriter.seek(fileWriter.length);         
-		var blob = new Blob([log], {type:'text/plain'});
-		console.log(blob);
+	logOb.createWriter(function(fileWriter) {       
+		var blob = new Blob([str], {type:'text/plain'});
 		fileWriter.write(blob);
-		alert("ok, in theory i worked");
 	}, fail);
+	
+	alert(str+' enregistré');
 }
 
-function justForTesting() {
+function readLog() {
 	logOb.file(function(file) {
 		var reader = new FileReader();
 
 		reader.onloadend = function(e) {
+			var res=this.result.split("\n");
+			alert(res);
+			nom.value=res[0];
+			prenom.value=res[1];
+			//birthday.value=res[2];
+			tel.value=res[2];
+			des.value=res[3];
 			alert(this.result);
 		};
-
 		reader.readAsText(file);
 	}, fail);
 
 }
+/*
+function justForTesting() {
+	logOb.file(function(file) {
+		var freader = new FileReader();
+
+		freader.onloadend = function(e) {
+			alert(this.result);
+		};
+
+		freader.readAsText(file);
+	}, fail);
+}*/
