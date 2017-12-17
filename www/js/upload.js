75dbc2d5fb47d023472035e4f11ef6effe6bfa71
julierbllrt <julie.robillart@isen.yncrea.fr>
2017-12-17 20:05:28 +0100

    avancé sur upload

--- www/js/upload.js
+++ www/js/upload.js
@@ -4,41 +4,48 @@ document.getElementById('select_fichier').addEventListener('touchend',copier, fa
 var category;
 var fileOb;
 var newDirOb;
+var oldDirOb;
 
 function fail(e) {
 	console.log("FileSystem Error");
 	console.log(e);
+	console.log("//////////////////////////////////////////");
 }
 
 function handleFileSelect(evt) {
 	var files = evt.target.files;
-	var f = files[0];
 	console.log(files);
+	var f = files[0];
+	console.log(f);
 	console.log(f.name);
 	var reader = new FileReader();
-	
+	console.log(f);
 	reader.onload = (function(theFile) {
 		return function(e) {
 			document.getElementById('list').innerHTML = ['<img src="', e.target.result,'" title="', theFile.name, '" />'].join('');
 		};
 	})(f);
+	console.log("//////////////////////////////////////////");
+	
+	
 	
 	
 	navigator.Env.getDirectory("Downloads", 
 		function (path) {
 			window.resolveLocalFileSystemURL("file:///storage/emulated/0/"+ path, function(dir) {
 				console.log("got main dir", dir);
+				oldDirOb=dir;
 				dir.getFile(f.name, {create:false}, function(file) {
 					fileOb=file;
 					console.log("got the file", file);
 					window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dirEntry) {
 						newDirOb=dirEntry
 						console.log(dirEntry);
-						alert(dirEntry.nativeURL+" dir");
-						
+						console.log("//////////////////////////////////////////");
 					}, fail);
 				});
 			});
+			
 		},
 		function (error) {
 			console.log("getDirectory error: " + error);
@@ -47,21 +54,31 @@ function handleFileSelect(evt) {
 
 	reader.readAsDataURL(f);
 	//console.log(f.toURL());
+	
 }
 
 function copier() {	
-	console.log(fileOb);
-	console.log(newDirOb);
-	console.log(document.getElementById("signification").value);
-
-	console.log(newDirOb);
-	alert(newDirOb.nativeURL+" dir");
-	fileOb.copyTo(newDirOb);
+	var newName = document.getElementById("signification").value+".png";
+	console.log(newName);
+	console.log("fichier selectionné:",fileOb);
+	console.log("dossier d'origine:",oldDirOb);
+	console.log("dossier destinataire:",newDirOb);
+	
+	fileOb.moveTo(newDirOb, newName);
+	alert(fileOb.name+" renommer");
+	
+	newDirOb.getFile(newName, {create:false}, function(file) {
+		fileOb=file;
+		alert(file.name+" nouveau nom");
+	});
+	
 	alert(fileOb.name+" copié");
-
-
+	console.log(fileOb);
+	
 	alert('getfile');
 }
+
+
 
 
 
