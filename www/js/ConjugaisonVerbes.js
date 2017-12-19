93cd2201ef30db1772e141788b079bd81f775adc
nicolasroi <nicolas.roi@isen.yncrea.fr>
2017-12-19 18:33:24 +0100

    conjugaison dans main-Nico.js

--- www/js/ConjugaisonVerbes.js
+++ www/js/ConjugaisonVerbes.js
@@ -14,63 +14,19 @@ var terminaisonsFutur = ["ai","as","a","ons","ez","ent"];
 
 
 
-function recupSujetFromDB (tx, results){
-	if(results.rows.item(0).personne!=null){
-		{mot:results.rows.item(0).mot,genre:results.rows.item(0).genre, personne:results.rows.item(0).personne};
+function recupSujetFromDB (results){
+	if(!results.rows.item(0).picto.includes("Verbes") && results.rows.item(0).genre!=null && results.rows.item(0).personne!=null){
+		var leSujet = {mot:results.rows.item(0).mot,genre:results.rows.item(0).genre, personne:results.rows.item(0).personne};
 	}
-	else{
-		var leMot=results.rows.item(0).mot.toLowerCase();
-		var laPersonne = 3;
-		var leGenre = false;
-		
+	
 
-		switch(leMot){
-			case "je":
-				laPersonne=1;
-				break;
-			case "moi":
-				laPersonne=1;
-				break;
-			case "tu":
-				laPersonne=2;
-				break;
-			case "il":
-				laPersonne=3;
-				break;
-			case "elle":
-				laPersonne=3;
-				leGenre = 1;
-				break;
-			case "nous":
-				laPersonne=4;
-				
-				break;
-			case "vous":
-				laPersonne=5;
-				
-				break;
-			case "ils":
-				laPersonne=6;
-				
-				break;
-			case "elles":
-				leGenre = 1;
-				
-				laPersonne=6;
-				break;
-			default :
-				laPersonne=3;
-				break;
-		}
-		var leSujet = {mot:leMot,genre:leGenre,personne:laPersonne};
-	}
 	return(leSujet);
 }
 		
 
 
 
-function recupVerbeFromDB (tx, results){
+function recupVerbeFromDB (results){
 	if(results.rows.item(0).picto.find("Verbes")!=-1){
 
 		// si c'est un verbe
@@ -104,7 +60,7 @@ function recupVerbeFromDB (tx, results){
 }
 // Conjugaison des verbes au présent
 
-function conjugaisonPresent(verbe, sujet){
+function conjugaisonPresent(sujet, verbe){
 
 // <test>
 //var sujet = {mot:"nous",genre:1,personne:};
@@ -116,100 +72,103 @@ function conjugaisonPresent(verbe, sujet){
 	var inf = verbe.infinitif;
 	var irregularPresent = [verbe.pres1,verbe.pres2,verbe.pres3,verbe.pres4,verbe.pres5,verbe.pres6];
 
-		
-	if(verbe.pres1==null && verbe.pres2==null && verbe.pres3==null && verbe.pres4==null && verbe.pres5==null && verbe.pres6==null){
-		switch(verbe.groupe){
-		
-			case 1: // conjugaison des verbes du 1er groupe
+	if(sujet.personne!=null){	
+		if(verbe.pres1==null && verbe.pres2==null && verbe.pres3==null && verbe.pres4==null && verbe.pres5==null && verbe.pres6==null){
+			switch(verbe.groupe){
+			
+				case 1: // conjugaison des verbes du 1er groupe
 
-				var radical = inf.substr(inf.length - 4); // récupération du radical de l'infinitif (les 4 derniers caractères)
+					var radical = inf.substr(inf.length - 4); // récupération du radical de l'infinitif (les 4 derniers caractères)
 
-				if (radical.includes('ger')){	// verbe en -ger
-					inf = inf.substr(0, inf.length - 3);
-					if(sujet.personne == 4){
-						inf = inf+'ge';
+					if (radical.includes('ger')){	// verbe en -ger
+						inf = inf.substr(0, inf.length - 3);
+						if(sujet.personne == 4){
+							inf = inf+'ge';
+						}
+						else if(sujet.personne < 4 || sujet.personne > 4){
+							inf = inf+'g';
+						}
 					}
-					else if(sujet.personne < 4 || sujet.personne > 4){
-						inf = inf+'g';
+
+					else if (radical.includes('cer')){ // verbe en -cer
+						inf = inf.substr(0, inf.length - 3);
+						if(sujet.personne == 4){
+							inf = inf+'ç';
+						}
+						else{
+							inf = inf+'c';
+						}
 					}
-				}
 
-				else if (radical.includes('cer')){ // verbe en -cer
-					inf = inf.substr(0, inf.length - 3);
-					if(sujet.personne == 4){
-						inf = inf+'ç';
+					else if (radical.includes('yer')){ // verbe en -oyer, -ayer, -uyer
+						inf = inf.substr(0, inf.length - 3);
+						if(sujet.personne == 4 || sujet.personne == 5){
+							inf = inf+'y';
+						}
+						else{
+							inf = inf+'i';
+						}
 					}
-					else{
-						inf = inf+'c';
+
+					else if (radical.includes('eler')){ // verbe en -eler
+						inf = inf.substr(0, inf.length - 3);
+						if(sujet.personne == 4 || sujet.personne == 5){
+							inf = inf+'l';
+						}
+						else{
+							inf = inf+'ll';
+						}
 					}
-				}
 
-				else if (radical.includes('yer')){ // verbe en -oyer, -ayer, -uyer
-					inf = inf.substr(0, inf.length - 3);
-					if(sujet.personne == 4 || sujet.personne == 5){
-						inf = inf+'y';
+					else if (radical.includes('eter')){ // verbe en -eter
+						inf = inf.substr(0, inf.length - 3);
+						if(sujet.personne == 4 || sujet.personne == 5){
+							inf = inf+'t';
+						}
+						else{
+							inf = inf+'tt';
+						}
 					}
-					else{
-						inf = inf+'i';
+					else if(radical.includes('éder') && (sujet.personne < 4 && sujet.personne > 5)){
+						inf = inf.replace('é','è');	
 					}
-				}
 
-				else if (radical.includes('eler')){ // verbe en -eler
-					inf = inf.substr(0, inf.length - 3);
-					if(sujet.personne == 4 || sujet.personne == 5){
-						inf = inf+'l';
+					else {
+						inf = inf.substr(0, inf.length - 2);
 					}
-					else{
-						inf = inf+'ll';
+
+
+					resultat = inf + terminaisons1erGroupe[sujet.personne-1]; // rajout de la terminaison pour la personne spécifiée
+					break;
+
+				case 2: // Conjugaison du 2e groupe
+
+					inf = inf.substr(0, inf.length - 1);	// suppression de -ir
+
+					if(verbe.infinitif=='haïr' && (sujet.personne < 4)){
+						inf = inf.replace('ï','i');
 					}
-				}
+					resultat = inf + terminaisons2emeGroupe[sujet.personne-1]; // rajout de la terminaison pour la personne spécifiée
 
-				else if (radical.includes('eter')){ // verbe en -eter
-					inf = inf.substr(0, inf.length - 3);
-					if(sujet.personne == 4 || sujet.personne == 5){
-						inf = inf+'t';
-					}
-					else{
-						inf = inf+'tt';
-					}
-				}
-				else if(radical.includes('éder') && (sujet.personne < 4 && sujet.personne > 5)){
-					inf = inf.replace('é','è');	
-				}
+					break; 
 
-				else {
-					inf = inf.substr(0, inf.length - 2);
-				}
+				case 3: // conjugaison du 3e groupe
+					break;
+					
+						// nous considérerons tous le 3ème groupe comme irrégulier, c'est plus simple...
+					
 
+			}		
 
-				resultat = inf + terminaisons1erGroupe[sujet.personne-1]; // rajout de la terminaison pour la personne spécifiée
-				break;
+		}
 
-			case 2: // Conjugaison du 2e groupe
-
-				inf = inf.substr(0, inf.length - 1);	// suppression de -ir
-
-				if(verbe.infinitif=='haïr' && (sujet.personne < 4)){
-					inf = inf.replace('ï','i');
-				}
-				resultat = inf + terminaisons2emeGroupe[sujet.personne-1]; // rajout de la terminaison pour la personne spécifiée
-
-				break; 
-
-			case 3: // conjugaison du 3e groupe
-				break;
-				
-					// nous considérerons tous le 3ème groupe comme irrégulier, c'est plus simple...
-				
-
-		}		
-
+		else{
+			resultat = irregularPresent[sujet.personne-1];
+		}
 	}
-
 	else{
-		resultat = irregularPresent[sujet.personne-1];
+		resultat = inf;
 	}
-
 	//document.getElementById("present").innerHTML = sujet.mot+" "+resultat;
 	return resultat;
 
@@ -217,7 +176,7 @@ function conjugaisonPresent(verbe, sujet){
 
 // Conjugaison des verbes au futur
 
-function conjugaisonFutur(verbe, sujet){
+function conjugaisonFutur(sujet, verbe){
 
 // <test>
 //var sujet = {mot:"nous",genre:1,personne:4};
@@ -228,56 +187,60 @@ function conjugaisonFutur(verbe, sujet){
 	var inf = verbe.infinitif;
 	inf.toLowerCase();
 
+	if(sujet.personne!=null){
+		if(verbe.irregularFutur==null || verbe.irregularFutur==''){ // si le verbe est régulier
+			
+			switch(verbe.groupe){
 
-	if(verbe.irregularFutur==null || verbe.irregularFutur==''){ // si le verbe est régulier
-		
-		switch(verbe.groupe){
+				case 1: //	1er groupe
+					var radical = inf.substr(inf.length - 4);
 
-			case 1: //	1er groupe
-				var radical = inf.substr(inf.length - 4);
+					if (radical.includes('eler')){ // radical en -eler
+						inf = inf.substr(0, inf.length - 2);
+						inf = inf+'l'+'er';
+					}
 
-				if (radical.includes('eler')){ // radical en -eler
-					inf = inf.substr(0, inf.length - 2);
-					inf = inf+'l'+'er';
-				}
+					if (radical.includes('yer')){ // radical en -yer
+						inf = inf.substr(0, inf.length - 3);
+						inf = inf+'ier';
+					}
 
-				if (radical.includes('yer')){ // radical en -yer
-					inf = inf.substr(0, inf.length - 3);
-					inf = inf+'ier';
-				}
+					if (radical.includes('eter')){ // radical en -eter
+						inf = inf.substr(0, inf.length - 2);
+						inf = inf+'t'+'er';
+					}
+			
 
-				if (radical.includes('eter')){ // radical en -eter
-					inf = inf.substr(0, inf.length - 2);
-					inf = inf+'t'+'er';
-				}
-		
+					break;
 
-				break;
+				case 2: // 2ème groupe
+					
+					break;
 
-			case 2: // 2ème groupe
-				
-				break;
+				case 3: // 3ème groupe
 
-			case 3: // 3ème groupe
+					var radical = inf.substr(inf.length - 3);
 
-				var radical = inf.substr(inf.length - 3);
+					if (radical.includes('re')){ // radical en -re
+						inf = inf.substr(0, inf.length -1 );
+					}
 
-				if (radical.includes('re')){ // radical en -re
-					inf = inf.substr(0, inf.length -1 );
-				}
+					else if (radical.includes('rir')){ // radical en -rir
+						inf = inf.substr(0, inf.length - 2);
+						inf = inf + 'r';
+					}
+					break;
 
-				else if (radical.includes('rir')){ // radical en -rir
-					inf = inf.substr(0, inf.length - 2);
-					inf = inf + 'r';
-				}
-				break;
-
+			}
+			resultat = inf + terminaisonsFutur[sujet.personne-1]; // rajout de la terminaison pour la personne spécifiée
 		}
-		resultat = inf + terminaisonsFutur[sujet.personne-1]; // rajout de la terminaison pour la personne spécifiée
-	}
 
-	else{	// si le verbe n'est pas régulier
-		resultat = verbe.irregularFutur + terminaisonsFutur[sujet.personne-1];
+		else{	// si le verbe n'est pas régulier
+			resultat = verbe.irregularFutur + terminaisonsFutur[sujet.personne-1];
+		}
+	}
+	else{
+		resultat = inf;
 	}
 	//document.getElementById("futur").innerHTML = sujet.mot+" "+resultat;
 	return resultat;
@@ -287,7 +250,7 @@ function conjugaisonFutur(verbe, sujet){
 
 //Conjugaison des verbes au passé
 
-function conjugaisonPasse(verbe, sujet){
+function conjugaisonPasse(sujet, verbe){
 	var avoir = ["ai","as","a","avons","avez","ont"];
 	var etre = ["suis","es","est","sommes","êtes","sont"]; 
 
@@ -302,46 +265,59 @@ function conjugaisonPasse(verbe, sujet){
 	participe.toLowerCase();
 
 	
+	if(sujet.personne!=null || sujet.genre!==null){
+		switch(verbe.auxiliaire){
 
-	switch(verbe.auxiliaire){
+			case "avoir":
+				aux = avoir[sujet.personne-1];
+				break;
 
-		case "avoir":
-			aux = avoir[sujet.personne-1];
-			break;
-
-		case "être":
-			aux = etre[sujet.personne-1];
-			if(participe.substr(participe.length - 2)=='us'){ // cas où le participe passé finit en -us
-				if(sujet.genre==1){
-					participe = participe.substr(0, participe.length - 1); //	suppression du 's' pour mettre un 't' à la place
-					participe = participe+'t';
-					if (sujet.personne<4){
-						participe = participe+'e';
-					}
-					else{
-						participe = participe+'es';
-					}
-				}
-			}
-			else{
-				if (sujet.genre==0){
-					if(sujet.personne>3 && participe.substr(participe.length - 1)!='s'){
-					participe = participe+'s';
+			case "être":
+				aux = etre[sujet.personne-1];
+				if(participe.substr(participe.length - 2)=='us'){ // cas où le participe passé finit en -us
+					if(sujet.genre==1){
+						participe = participe.substr(0, participe.length - 1); //	suppression du 's' pour mettre un 't' à la place
+						participe = participe+'t';
+						if (sujet.personne<4){
+							participe = participe+'e';
+						}
+						else{
+							participe = participe+'es';
+						}
 					}
 				}
 				else{
-					if(sujet.personne<4){
-					participe = participe+'e';
+					if (sujet.genre==0){
+						if(sujet.personne>3 && participe.substr(participe.length - 1)!='s'){
+						participe = participe+'s';
+						}
 					}
 					else{
-						participe = participe+'es'
+						if(sujet.personne<4){
+						participe = participe+'e';
+						}
+						else{
+							participe = participe+'es'
+						}
 					}
 				}
-			}
-			break;			
+				break;			
+		}
+		resultat = aux+" "+participe;
+	}
+	else{
+		resultat = verbe.auxiliaire+" "+participe;
 	}
 	
-	resultat = aux+" "+participe;
 	//document.getElementById("passé").innerHTML = resultat;
 	return resultat;
 }
+
+
+function conjugaison(tx, results){
+	var previousPicto = results.rows.item(0);
+}
+
+
+
+
---
+++ www/js/main-Nico.js
@@ -0,0 +1,315 @@
+function include(fileName){
+  document.write("<script type='text/javascript' src='"+fileName+"'></script>" );
+}
+
+include('ConjugaisonVerbes.js');
+
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+var favOb;
+var previousPicto;
+
+function errorCB(err) {
+ 	alert("Error processing SQL: "+err.code);
+ }
+
+function fail(e) {
+	console.log("FileSystem Error");
+	console.log(e);
+}
+
+function onDeviceReady(){
+	window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dir) {
+		console.log(dir.nativeURL);
+		console.log("got main dir", dir);
+		dir.getFile("fav.txt", {create:true, exclusive: false}, function(file) {
+			console.log("got the file", file);
+			favOb = file;		
+			console.log('getfile');	
+		});
+	});
+	//setApropos();
+	//setFav();
+}
+
+
+// Click Picto
+//
+
+
+
+function onClickPicto(elmnt) {
+	var db = window.openDatabase("Database", "1.0", "Picto Demo", 200000);
+	db.transaction(function(tx){queryDbClickPicto(tx, elmnt)}, errorCB);
+}
+
+function queryDbClickPicto(tx, id) {
+	var q = "";
+	var button = document.getElementById(id);
+ 	var catElement = button.firstElementChild;
+	var category = catElement.className;
+	var imgElement = button.getElementsByTagName('img');
+	var imgSrc = imgElement[0].id;
+	if (category == "Verbes"){
+		q += 'SELECT * FROM VERBES WHERE id LIKE ? ';
+	}
+	else {
+		q += 'SELECT * FROM PICTOS WHERE id LIKE ? ';
+	}
+	tx.executeSql(q, [imgSrc], querySuccessClickPicto, errorCB);
+}
+
+function querySuccessClickPicto(tx, results) {
+	var currentPicto = results.rows.item(0);
+	var monSujet;
+	var monVerbe;
+
+	if(!previousPicto.picto.includes("Verbes")){
+		monSujet = recupSujetFromDB(previousPicto);
+	}
+	
+	if(currentPicto.picto.includes("Verbes")){
+		monVerbe = recupVerbeFromDB(currentPicto);
+	}
+
+	var txtPicto = " " + conjugaisonPresent(monSujet,monVerbe);
+	document.getElementById("saisie").value += txtPicto;
+	previousPicto = results.rows.item(0);
+}
+
+function onClickPictoTxt(id){
+	var button = document.getElementById(id);
+	var txtPicto = button.firstElementChild.innerHTML;
+	document.getElementById("saisie").value += txtPicto;
+}
+
+
+
+ // Click Category
+ //
+
+function onClickCategory(category){
+	//alert("going onclickcategory");
+ 	var db = window.openDatabase("Database", "1.0", "Picto Demo", 200000);
+ 	db.transaction(function(tx){queryDbClickCategory(tx, category)}, errorCB);
+ }
+ 
+ function queryDbClickCategory(tx, category){
+ 	//alert("going querydb");
+ 	var q = "";
+ 	if (category == "Verbes"){
+ 		q += 'SELECT * FROM VERBES WHERE picto LIKE ?';
+ 	}
+ 	else{
+ 		q += 'SELECT * FROM PICTOS WHERE picto LIKE ?';
+ 	}
+ 	tx.executeSql(q, ['img/'+category+'/%'], function(tx, results){querySuccessClickCategory(tx, results, category)}, errorCB);
+ }
+ 
+ function querySuccessClickCategory(tx, results, category){
+	//alert("going querysuccess");
+	var imgPicto="";
+ 	var len = results.rows.length;
+ 	if (len >= 36){
+ 		len = 36;
+ 	}
+ 	for (var i = 0; i < 36; i++){
+		document.getElementById("picto"+i).innerHTML="";
+	}
+	for (var k = 0; k < len; k++) {
+		var actualbutton = document.getElementById("picto"+k);
+		actualbutton.innerHTML = imgPicto;
+			if(actualbutton.hasAttribute('class')){
+				actualbutton.removeAttribute('class');
+				if (document.getElementById("deletemode").innerHTML=="GO DELETE"){
+					actualbutton.setAttribute('onclick', 'onClickPicto(this.id)');
+				}
+				else{
+					actualbutton.setAttribute('onclick', 'goDelete(this.id)');
+				}
+			}
+ 		imgPicto +='<div class="'+ category +'" id="test'+k+'"><img id="' + results.rows.item(k).id + '" class="picto" src="' + results.rows.item(k).picto + '"><p class="mot">' + results.rows.item(k).mot + '</p>'+'<img class="cross" src="icon/delete.png">'+'</div>';
+ 		actualbutton.innerHTML = imgPicto;
+ 		imgPicto="";
+
+ 	}
+	for(var j = len; j < 36; j++){
+		var actualbutton = document.getElementById("picto"+j);
+		if(!actualbutton.hasAttribute('class')){
+			actualbutton.setAttribute('onclick', 'displayDispo()');
+			actualbutton.setAttribute('class', 'dispo');
+		}
+ 	}
+ 	if(document.getElementById("deletemode").innerHTML=="STOP DELETE"){
+ 		$('.cross').css("display", "block");
+ 	}
+ 	else{
+ 		$('.cross').css("display", "none");
+ 	}
+
+ }
+ 
+
+
+
+ // Delete in DB
+ //
+
+function godeletemode(){
+	var buttondelete = document.getElementById("deletemode");
+	buttondelete.setAttribute('onclick', 'stopdeletemode()');
+	buttondelete.innerHTML="STOP DELETE";
+	$('.cross').css("display", "block");
+	for (var i=0; i<36; i++){
+		var actualbutton = document.getElementById("picto"+i);
+		if (actualbutton.className!="dispo"){
+			actualbutton.setAttribute('onclick', 'goDelete(this.id)');
+		}
+	}
+	
+}
+
+function stopdeletemode(){
+	var buttondelete=document.getElementById("deletemode");
+	buttondelete.setAttribute('onclick', 'godeletemode()');
+	buttondelete.innerHTML="GO DELETE";
+	$('.cross').css("display", "none");
+	for (var i=0; i<36; i++){
+		var actualbutton = document.getElementById("picto"+i);
+		if (actualbutton.className!="dispo"){
+			actualbutton.setAttribute('onclick', 'onClickPicto(this.id)');
+		}
+	}
+}
+
+ function goDelete(id){
+ 	//alert("go delete");
+ 	var db = window.openDatabase("Database", "1.0", "Picto Demo", 200000);
+ 	if (confirm("Confirmer la suppression du picto ?")==true){
+ 		db.transaction(function(tx){querydbDelete(tx, id)}, errorCB);
+ 	}
+ }
+
+ function querydbDelete(tx, id){
+ 	//alert("going querydbdelete");
+ 	var button = document.getElementById(id);
+ 	var catElement = button.firstElementChild;
+	var category = catElement.className;
+	var imgElement = button.getElementsByTagName('img');
+	var imgSrc = imgElement[0].id;
+	tx.executeSql('DELETE FROM PICTOS WHERE id = ?', [imgSrc], onClickCategory(category), errorCB);
+ }
+
+
+// Set A propos
+//
+
+function setApropos(){
+	var txtPicto = "";
+	var nom = document.getElementById('nom');
+	var prenom = document.getElementById('prenom');
+	var birthday = document.getElementById('birthday');
+	var tel = document.getElementById('tel');
+	var des = document.getElementById('description');
+
+	if(nom.value!="" && prenom.value!=""){
+		txtPicto += "<p>Je m'appelle " + prenom.value + " " + nom.value + "</p>";
+		alert(txtPicto);
+		document.getElementById("apropos0").innerHTML = txtPicto;
+		txtPicto = "";
+	}
+
+	if(birthday.value!=""){
+		txtPicto += "<p>J'ai </p>";
+		document.getElementById("apropos1").innerHTML = txtPicto;
+		txtPicto = "";
+	}
+
+	if (tel!=""){
+		txtPicto += "<p>Mon numéro de téléphone est le " + tel.value + "</p>";
+		document.getElementById("apropos2").innerHTML = txtPicto;
+		txtPicto = "";
+	}
+
+}
+
+function CalculAge(birthday) {
+    var td=new Date();
+    var dtn=birthday;
+    var an=dtn.substr(6,4);
+    var mois=dtn.substr(3,2);
+    var day= dtn.substr(0,2);
+    var age=td.getFullYear()-an;
+ 
+    var mMois=td.getMonth()-mois+1;
+ 
+     
+    if(mMois < 0)
+    {
+        age=age-1;
+    }  
+    else
+    {
+        if(mMois == 0)
+        {
+            var mDate=td.getDay()-day+1;
+            if(mDate < 0)
+            {
+                age=age-1;
+            }
+             
+        }
+    }
+    return age;
+}
+
+
+
+//  Favoris
+//
+
+function setFav(){
+	favOb.file(function(file) {
+		var reader = new FileReader();
+		var txtPicto = "";
+
+		reader.onloadend = function(e) {
+			var res=this.result.split("\n");
+			for (var i=0; i<res.length; i++){
+				var favToSet = document.getElementById("fav"+i); 
+				txtPicto += '<p>'+res[i]+'</p>';
+				favToSet.innerHTML = txtPicto;
+				txtPicto = "";
+			}
+		};
+		reader.readAsText(file);
+	}, fail);
+
+}
+
+
+function newFav(){
+	var toAdd = document.getElementById("saisie").value;
+	toAdd += "\n"
+	var favStr = "";
+	favOb.file(function(file) {
+		var reader = new FileReader();
+
+		reader.onloadend = function(e) {
+			favStr += this.result;
+			
+		};
+		reader.readAsText(file);
+	}, fail);
+	favStr += toAdd;
+
+	if(!favOb){
+		return;
+	} 
+	favOb.createWriter(function(fileWriter) {       
+		var blob = new Blob([favStr], {type:'text/plain'});
+		fileWriter.write(blob);
+	}, fail);
+}
